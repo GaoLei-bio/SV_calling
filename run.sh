128762e392ed6fe568ed26689c5f1f6fabfb83cc
Lei Gao <32151560+GaoLei-bio@users.noreply.github.com>
2019-07-16 13:03:45 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-
-
-ln -s /data/lei/Tomato_genomes/00_Genome/SL4.0.genome.fasta .
-ln -s /data/lei/Tomato_genomes/00_Genome/Pimp_v1.4.fasta .
-
-
-bash SV_pipeline_2.sh  SL4.0.genome.fasta  Pimp_v1.4.fasta   SP2SL   24 
-
